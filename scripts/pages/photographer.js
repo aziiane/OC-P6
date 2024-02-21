ad6c058b6e9152142d6e9895ac2490680b00ae51
Aziz Chebil <chebilaziz16@gmail.com>
2024-02-21 12:27:48 +0100

    Photographers data fetching + begining profile

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,48 @@
-//Mettre le code JavaScript lié à la page photographer.html
+    //Mettre le code JavaScript lié à la page photographer.html
+    async function getPhotographerData(id) {
+    let photographerData = await fetch("data/photographers.json").then(
+        (response) => response.json()
+    );
+    let photographer = photographerData.photographers.find(
+        (photographer) => photographer.id === id
+    );
+    return photographer;
+    }
+
+    async function displayData(photographer) {
+        const picture = `assets/photographers/${photographer.portrait}`;
+        const photographerHeader = document.querySelector(".photograph-header");
+        const photographerInformation = document.querySelector(
+            ".photograph-informations"
+        );
+        console.log("photographer => ", photographer);
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `${photographer.name.toLowerCase().replace(" ", "-")}-profile-picture`)
+        img.setAttribute("aria-label", `${photographer.name.toLowerCase().replace(" ", "-")}-profile-picture`)
+
+        const h2 = document.createElement("h2");
+        h2.textContent = photographer.name;
+
+        const location = document.createElement("p");
+        location.innerText = `${photographer.city}, ${photographer.country}`;
+        location.classList.add("location");
+
+        const tagLine = document.createElement("p");
+        tagLine.innerText = photographer.tagline;
+        tagLine.classList.add("tagline");
+
+        photographerHeader.appendChild(img);
+        photographerInformation.appendChild(h2);
+        photographerInformation.appendChild(location);
+        photographerInformation.appendChild(tagLine);
+    }
+
+    async function init() {
+    const params = await new URL(document.location).searchParams;
+    const photographerId = parseInt(params.get("id"));
+    const photographer = await getPhotographerData(parseInt(photographerId));
+    displayData(photographer);
+    }
+
+    init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,6 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price} = data;
+    console.log("LA DAAATA => ", data)
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,10 +8,25 @@ function photographerTemplate(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", `${name.toLowerCase().replace(" ", "-")}-profile-picture`)
+        img.setAttribute("aria-label", `${name.toLowerCase().replace(" ", "-")}-profile-picture`)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const location = document.createElement( 'p' );
+        location.innerText = `${city}, ${country}`;
+        location.classList.add('location');
+        const tagLine = document.createElement( 'p' );
+        tagLine.innerText = tagline;
+        tagLine.classList.add('tagline');
+        const priceTag = document.createElement( 'p' );
+        priceTag.innerText = `${price}€/jour`;
+        priceTag.classList.add('price');
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tagLine);
+        article.appendChild(priceTag);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
